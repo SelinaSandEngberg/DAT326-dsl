b9590f154b26aa9a6d61dc599288d4235de65e03
OscarDahlqvist <oscdah@student.chalmers.se>
2021-01-27 14:31:17 +0100

    part1done

--- uppgift.hs
+++ uppgift.hs
@@ -66,6 +66,7 @@ PRED should have contructors for
 -}
 {-# LANGUAGE GADTs #-}
 import Data.List
+import Test.QuickCheck
 
 data TERM v where 
   Empty         :: TERM v
@@ -74,11 +75,6 @@ data TERM v where
   Intersection  :: TERM v -> TERM v -> TERM v
   Value         :: v -> TERM v
  deriving Show
-{-
-instance Arbitrary Term v where
- arbitrary = do
-   r <- 
- -}
  
 data PRED v where
   Elem      :: v -> TERM v -> PRED v
@@ -98,16 +94,49 @@ type Env var dom = [(var,dom)]
 eval :: (Eq v) => (Env v Set) -> TERM v -> Set
 eval env (Empty)                = S []
 eval env (Singleton t)          = S [eval env t]
-eval env (Union t1 t2)          = S $ nub $ [eval env t1]++[eval env t2]
+eval env (Union t1 t2)          = S $ nub $ list1 ++ list2
+ where 
+  list1 = setAsList $ eval env t1
+  list2 = setAsList $ eval env t2
+  
 eval env (Intersection t1 t2)   = S $ filter (\x -> x `elem` list2) list1
  where 
-  list1 = [eval env t1]
-  list2 = [eval env t2]
-eval env (Value name)           = head [set | (varId,set)<-env, varId==name]
+  list1 = setAsList $ eval env t1
+  list2 = setAsList $ eval env t2
+eval env (Value name)           = head [set | (varId,set)<-env, varId==name] --get value from env
 
 --PART 2.check
---check :: (Eq v) => (Env v Set) -> PRED v -> Bool
---FUNKAR INTE
---check env (Elem v t) = elem (S v) $ setAsList (eval env t)
+getFromEnv :: (Eq v) => (Env v x) -> v -> x
+getFromEnv env v = head [envSet | (varId,envSet)<-env, varId==v]
 
 
+(-->) :: Bool -> Bool -> Bool
+False --> _ = True
+True  --> x = x
+
+check :: (Eq v) => (Env v Set) -> PRED v -> Bool
+check env (Elem v t) = set `elem` (setAsList $ eval env t)
+ where set = getFromEnv env v
+ 
+-- true if t1 ⊂ t2
+check env (Subset t1 t2) = and $ map (\x -> x `elem` s2) s1
+ where
+  s1 = setAsList $ eval env t1
+  s2 = setAsList $ eval env t2
+  
+check env (And p1 p2)       = check env p1 && check env p2
+check env (Or p1 p2)        = check env p1 || check env p2
+check env (Implies p1 p2)   = check env p1 --> check env p2
+check env (Not p)           = not $ check env p
+
+--PART 3.vonNeumann
+vonNeumann :: Integer -> TERM Integer
+vonNeumann 0 = Empty
+vonNeumann n = Union child (Singleton child)
+ where child = vonNeumann (n-1)
+
+claim1 :: Integer -> Integer -> Bool
+claim1 n1 n2 = (n1 <= n2) --> (check [] ((vonNeumann n1) `Subset` (vonNeumann n1)))
+
+claim2 n = (eval [] $ vonNeumann n) == set
+ where set = S $ map (\x -> eval [] $ vonNeumann x) [0..(n-1)]
